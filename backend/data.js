47a6a956793c34d42fc796cc084aed4725086ed0
Binh Nguyen <Binh.dai.nguyen.97@gmail.com>
2020-09-16 04:04:27 -0700

    added redux, redux-thunk, store.js, productReducers.js

---
+++ backend/data.js
@@ -0,0 +1,11 @@
+export default {
+    products: [{
+        _id:'123',
+        name: 'Smol bois',
+        image: '/images/d1.jpg',
+        price: 50,
+        brand: 'Animez', 
+        rating: 5.8,
+        numReviews: 10
+    }]
+}
