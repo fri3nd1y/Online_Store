c465a9a7d6cd74b9002908b5c7a9cda0bf46728d
Binh Nguyen <Binh.dai.nguyen.97@gmail.com>
2020-09-18 05:19:40 -0700

    added more reducers, big problem with get images and product page :(

--- backend/data.js
+++ backend/data.js
@@ -6,6 +6,27 @@ export default {
         price: 50,
         brand: 'Animez', 
         rating: 5.8,
-        numReviews: 10
+        numReviews: 10,
+        countInStock: 5,
+    },
+    {
+        _id:'124',
+        name: 'Big Chungus',
+        image: '/images/d3.jpg',
+        price: 7,
+        brand: 'AnimezBois', 
+        rating: 8.8,
+        numReviews: 985,
+        countInStock: 0,
+    },
+    {
+        _id:'125',
+        name: 'Smol bois',
+        image: '/images/d2.jpg',
+        price: 50,
+        brand: 'Animez', 
+        rating: 5.8,
+        numReviews: 10,
+        countInStock: 5,
     }]
 }
